ee50b1378c836895ca5bd8ed119329fb4f3615c5
Glitch (glitch-blank-website) <none>
2023-02-27 02:11:48 +0000

    🚤😄 Checkpoint ./style.css:43232669/985 ./script.js:43232669/1135 ./.glitch-assets:43232669/382 ./index.html:43232669/2873

--- script.js
+++ script.js
@@ -1,4 +1,40 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+var pc = 2;
+var currentVideo = 0;
+var videos = document.querySelectorAll(".active,.inactive");
 
-console.log("Hello, world!");
+Array.from(videos).forEach(item => {
+    item.addEventListener('ended',myHandler,false);
+})
+console.log(currentVideo+1);
+
+function myHandler(e) {
+    var temp = Math.floor(Math.random() * 10);
+    console.log(temp+" "+pc);
+    if(temp<pc&&currentVideo==3){
+        if(temp<pc){
+            pc=2;
+            videos[currentVideo].className="inactive";
+            videos[0].className="active";
+            videos[0].play();
+            currentVideo=0;
+            console.log(currentVideo+1);
+        }
+        else{
+            pc++;
+            videos[currentVideo].play();
+        }
+        return;
+    }
+    if(temp<pc){
+        pc=2;
+        videos[currentVideo].className="inactive";
+        videos[currentVideo+1].className="active";
+        videos[currentVideo+1].play();
+        currentVideo++;
+        console.log(currentVideo+1);
+    }
+    else{
+        pc++;
+        videos[currentVideo].play();
+    }
+}
